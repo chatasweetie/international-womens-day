f8e71e2e087d311f1b5f684474163ad3b35fbfbd
Jessica <jessica.dene.earley@gmail.com>
2018-01-07 17:03:30 -0800

    added map hidden map

--- static/js/googlemap.js
+++ static/js/googlemap.js
@@ -1,15 +1,39 @@
 "use strict";
-var image = 'http://maps.google.com/mapfiles/marker_purple.png';
-var user = 'http://maps.google.com/mapfiles/dd-start.png';
+var image = 'http://www.googlemapsmarkers.com/v1/1CE9B6';
+// var user = 'http://maps.google.com/mapfiles/dd-start.png';
 
 
-function init(){
-    var mapDiv = document.getElementById("map");
-    var mapOptions= {
-        center: new google.maps.LatLng(37.757189, -122.335684),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(mapDiv, mapOptions);
+// function init(){
+//     var mapDiv = document.getElementById("map");
+//     var mapOptions= {
+//         center: new google.maps.LatLng(37.757189, -122.335684),
+//         zoom: 10,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
+//     var map = new google.maps.Map(mapDiv, mapOptions);
 
-    var infoWindow = new google.maps.InfoWindow({map: map});
+//     var infoWindow = new google.maps.InfoWindow({map: map});
+
+function initMap() {
+        var center = {lat: 37.789527, lng: -122.394276};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: center,
+          scrollwheel: false,
+          mapTypeControl: false,
+        });
+        var marker = new google.maps.Marker({
+          position: center,
+          map: map,
+          icon: image,
+          title:"Google Launch Pad",
+        });
+      }
+
+
+
+
+
+
+
+
