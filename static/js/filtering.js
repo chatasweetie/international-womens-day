3436ea50164251eab6ba135c5864677ac16da14e
Jessica <jessica.dene.earley@gmail.com>
2018-02-10 21:06:49 -0800

    added filtering and top button to talks and schdule

--- static/js/filtering.js
+++ static/js/filtering.js
@@ -1,16 +1,34 @@
-var $mediaElements = $('.link-card-pad');
+var $mediaElements = $('.card-block');
 
 $('.filter_link').click(function(e){
     e.preventDefault();
-    console.log("active")
-    // get the category from the attribute
+    
     var filterVal = $(this).data('filter');
-
-    if(filterVal === 'all'){
-      $mediaElements.show();
-    }else{
-       // hide all then filter the ones to show
-       $mediaElements.hide().filter('.' + filterVal).show();
-    }
+    $mediaElements.filter('.' + filterVal).toggleClass("schedule-session-highlight");
 });
 
+var $sessionElements = $('.schedule-talks');
+
+$('.filter_link').click(function(e){
+    e.preventDefault();
+
+    var filterVal = $(this).data('filter');
+    $sessionElements.filter('.' + filterVal).toggleClass("schedule-session-highlight");
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
